728019e5d42d6ceda67d3e9e2cdfbd0a45e4e6a3
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-11-04 00:23:00 +0300

    modify repository

--- app/src/main/java/com/sammy/koin/data/api/ApiHelperImpl.kt
+++ app/src/main/java/com/sammy/koin/data/api/ApiHelperImpl.kt
@@ -3,7 +3,7 @@ package com.sammy.koin.data.api
 import com.sammy.koin.data.model.User
 import retrofit2.Response
 
-class ApiHelperImpl (val apiService: ApiService):ApiHelper{
+class ApiHelperImpl (private val apiService: ApiService):ApiHelper{
     override suspend fun getUsers(): Response<List<User>> =
         apiService.getUsers()
 
--- app/src/main/java/com/sammy/koin/data/repository/MainRepository.kt
+++ app/src/main/java/com/sammy/koin/data/repository/MainRepository.kt
@@ -1,2 +1,7 @@
 package com.sammy.koin.data.repository
 
+import com.sammy.koin.data.api.ApiHelperImpl
+
+class MainRepository (private val apiHelperImpl: ApiHelperImpl){
+    suspend fun getUsers() = apiHelperImpl.getUsers()
+}
