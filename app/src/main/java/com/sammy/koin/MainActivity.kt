e378805ce74fe28c0c22e1687bb100747c3682fd
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-11-03 23:54:59 +0300

    create ui

--- app/src/main/java/com/sammy/koin/MainActivity.kt
+++ app/src/main/java/com/sammy/koin/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.sammy.koin
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
