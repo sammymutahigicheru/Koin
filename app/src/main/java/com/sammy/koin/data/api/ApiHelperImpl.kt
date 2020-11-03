2a61a02a65736f16195bdf3d2508f8f76fd59382
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-11-04 00:15:32 +0300

    modify apihelperImpl class

--- app/src/main/java/com/sammy/koin/data/api/ApiHelperImpl.kt
+++ app/src/main/java/com/sammy/koin/data/api/ApiHelperImpl.kt
@@ -1,2 +1,10 @@
 package com.sammy.koin.data.api
 
+import com.sammy.koin.data.model.User
+import retrofit2.Response
+
+class ApiHelperImpl (val apiService: ApiService):ApiHelper{
+    override suspend fun getUsers(): Response<List<User>> =
+        apiService.getUsers()
+
+}
