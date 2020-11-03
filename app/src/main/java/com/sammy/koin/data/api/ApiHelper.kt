a254f0c9aaa121d0c3e13ff3d3b24fbf87168009
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-11-04 00:13:22 +0300

    modify apihelper interface

--- app/src/main/java/com/sammy/koin/data/api/ApiHelper.kt
+++ app/src/main/java/com/sammy/koin/data/api/ApiHelper.kt
@@ -1,5 +1,8 @@
 package com.sammy.koin.data.api
 
+import com.sammy.koin.data.model.User
+import retrofit2.Response
+
 interface ApiHelper {
-    
+    suspend fun getUsers():Response<List<User>>
 }
