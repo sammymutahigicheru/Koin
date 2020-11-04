8077aa4072566e470258f3235dd6ce183f5f91d3
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-11-04 09:52:25 +0300

    create an instance of mainViewModel

--- app/src/main/java/com/sammy/koin/ui/MainActivity.kt
+++ app/src/main/java/com/sammy/koin/ui/MainActivity.kt
@@ -1,8 +1,15 @@
-package com.sammy.koin
+package com.sammy.koin.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sammy.koin.R
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    /*
+    * by viewModel() creates the instance for the ViewModel
+    * */
+    private val mainViewModel:MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
