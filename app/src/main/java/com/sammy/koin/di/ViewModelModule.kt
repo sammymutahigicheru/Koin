71ec8246c7c7304b09292ff76f97c0bde4a11124
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-11-04 10:16:59 +0300

    fix viewmodel injection

--- app/src/main/java/com/sammy/koin/di/ViewModelModule.kt
+++ app/src/main/java/com/sammy/koin/di/ViewModelModule.kt
@@ -1,8 +1,9 @@
 package com.sammy.koin.di
 
 import com.sammy.koin.ui.MainViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    single { MainViewModel(get(),get()) }
+    viewModel { MainViewModel(get(),get()) }
 }
