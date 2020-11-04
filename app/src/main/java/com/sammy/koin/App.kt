cea0d6c1a02f2b0a19704e4b1aec32de65956892
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-11-04 10:08:49 +0300

    modify app controller

--- app/src/main/java/com/sammy/koin/App.kt
+++ app/src/main/java/com/sammy/koin/App.kt
@@ -1,7 +1,18 @@
 package com.sammy.koin
 
 import android.app.Application
+import com.sammy.koin.di.appModule
+import com.sammy.koin.di.repoModule
+import com.sammy.koin.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App: Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(appModule, repoModule, viewModelModule))
+        }
+    }
 }
